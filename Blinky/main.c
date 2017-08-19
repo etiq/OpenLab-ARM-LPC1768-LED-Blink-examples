9bcb63b90e97c33f637747c058e00592aa851039
Kevin Mathews <kevin.mathews@etiq.in>
2017-08-19 12:14:41 +0530

    Blink LED example code for LPC1768

--- Blinky/main.c
+++ Blinky/main.c
@@ -1,9 +1,19 @@
+/*
+ * File name            : main.c
+ * Compiler             : MDK-Lite
+ * IDE                  : Keil uVision V5
+ * Author               : Etiq Technologies
+ * Processor            : LPC1768
+ * Created on           : June 09, 2017, 9:44 AM
+ * Description          : LED Blink Program
+ */
+
 #include <lpc17xx.h>   
 
-#define LCD_DATA_DIR	   LPC_GPIO1->FIODIR
-#define LCD_DATA_SET	   LPC_GPIO1->FIOSET
-#define LCD_DATA_CLR	   LPC_GPIO1->FIOCLR
-#define LCD_DATA_PIN		 LPC_GPIO1->FIOPIN
+#define DATA_DIR	   LPC_GPIO1->FIODIR
+#define DATA_SET	   LPC_GPIO1->FIOSET
+#define DATA_CLR	   LPC_GPIO1->FIOCLR
+#define DATA_PIN		 LPC_GPIO1->FIOPIN
 
 #define PIN_D0 24
 #define PIN_D1 25
@@ -26,13 +36,13 @@ int main()
 {
 	int i;
 	SystemInit();                    
-	LCD_DATA_DIR = (0xFF << PIN_D0);
+	DATA_DIR = (0xFF << PIN_D0);
 	i = PIN_D0;
 	while(1)
     {
-		LCD_DATA_SET = (0x01 << i);
+		DATA_SET = (0x01 << i);
 			delay_ms(250);
-		LCD_DATA_CLR = (0x01 << i);
+		DATA_CLR = (0x01 << i);
 		i++;
 			if(i > PIN_D7)
 				i = PIN_D0;
